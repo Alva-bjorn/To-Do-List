b070b912a0d0d08f7d96c3375a0e3f776e7db0e9
Alva <alva.bjornstrom@hotmail.se>
2026-09-21 13:46:14 +0200

    Initialize Electron app with main window setup

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 257,
+    height: 350,
+
+    resizable: false,
+    maximizable: false,
+    fullscreenable: false,
+    frame: false, 
+    transparent: true,
+    
+    webPreferences: {
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
